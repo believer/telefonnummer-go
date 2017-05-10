a0cb37cdb7e8b32e034c0ff88b1e922ae0c07fb9
believer <rickard.laurin@gmail.com>
2017-05-10 19:12:36 +0200

    Refactored to helper functions, 100% coverage, make regex compiler much much simpler

--- telefonnummer.go
+++ telefonnummer.go
@@ -1,117 +1,111 @@
 package telefonnummer
 
 import (
-  "strings"
-  "regexp"
+	"regexp"
+	"strconv"
+	"strings"
 )
 
 func normalize(phoneNumber string) string {
-  validNumbers := regexp.MustCompile(`\D`)
+	validNumbers := regexp.MustCompile(`\D`)
 
-  if strings.Contains(phoneNumber, "(0)") {
-    phoneNumber = strings.Replace(phoneNumber, "(0)", "", -1)
-  }
+	if strings.Contains(phoneNumber, "(0)") {
+		phoneNumber = strings.Replace(phoneNumber, "(0)", "", -1)
+	}
 
-  normalized := validNumbers.ReplaceAllString(phoneNumber, "")
+	normalized := validNumbers.ReplaceAllString(phoneNumber, "")
 
-  if normalized[:2] == "46" {
-    return "0" + normalized[2:]
-  }
+	if normalized[:2] == "46" {
+		return "0" + normalized[2:]
+	}
 
-  return normalized
+	return normalized
 }
 
 func areaCodeDigitCount(phoneNumber string) int {
-  doubleDigit := regexp.MustCompile(`^08`)
-  tripleDigit := regexp.MustCompile(`^0(1[013689]|2[0136]|3[1356]|4[0246]|54|6[03]|7[0235-9]|9[09])`)
+	doubleDigit := regexp.MustCompile(`^08`)
+	tripleDigit := regexp.MustCompile(`^0(1[013689]|2[0136]|3[1356]|4[0246]|54|6[03]|7[0235-9]|9[09])`)
 
-  if doubleDigit.MatchString(phoneNumber) {
-    return 2
-  } else if tripleDigit.MatchString(phoneNumber) {
-    return 3
-  }
+	if doubleDigit.MatchString(phoneNumber) {
+		return 2
+	} else if tripleDigit.MatchString(phoneNumber) {
+		return 3
+	}
 
-  return 4
+	return 4
+}
+
+func convertToString(number int) string {
+	return strconv.Itoa(number)
 }
 
 func makeRegex(areaCode int) func(int) *regexp.Regexp {
-  return func(firstDigits int) *regexp.Regexp {
-    if areaCode == 2 {
-      if firstDigits == 3 {
-        return regexp.MustCompile(`^(\d{2})(\d{3})(\d{2})(\d{2})$`)
-      }
+	return func(firstDigits int) *regexp.Regexp {
+		ac := convertToString(areaCode)
+		digits := convertToString(firstDigits)
+		standardFormat := "^(\\d{" + ac + "})(\\d{" + digits + "})(\\d{2})(\\d{2})$"
 
-      if firstDigits == 10 {
-        return regexp.MustCompile(`^(\d{2})(\d{3})(\d{3})(\d{2})$`)
-      }
+		if firstDigits == 10 {
+			return regexp.MustCompile(`^(\d{2})(\d{3})(\d{3})(\d{2})$`)
+		}
 
-      return regexp.MustCompile(`^(\d{2})(\d{2})(\d{2})(\d{2})$`)
-    }
+		return regexp.MustCompile(standardFormat)
+	}
+}
 
-    if areaCode == 3 {
-      if firstDigits == 3 {
-        return regexp.MustCompile(`^(\d{3})(\d{3})(\d{2})(\d{2})$`)
-      }
+func shortFormat(areaCode int, phoneNumber string) string {
+	ac := convertToString(areaCode)
+	shortRegex := "^(\\d{" + ac + "})(\\d{3})(\\d{2})$"
 
-      return regexp.MustCompile(`^(\d{3})(\d{2})(\d{2})(\d{2})$`)
-    }
+	return regexp.MustCompile(shortRegex).ReplaceAllString(phoneNumber, "$1-$2 $3")
+}
 
-    if areaCode == 4 {
-      return regexp.MustCompile(`^(\d{4})(\d{2})(\d{2})(\d{2})$`)
-    }
-
-    return regexp.MustCompile(`^(\d{3})(\d{3})(\d{2})(\d{2})$`)
-  }
+func normalFormat(regex *regexp.Regexp, phoneNumber string) string {
+	return regex.ReplaceAllString(phoneNumber, "$1-$2 $3 $4")
 }
 
 // Parse - Parses a telephone number
 func Parse(phoneNumber string) string {
-  var result string
+	normalized := normalize(phoneNumber)
+	areaCode := areaCodeDigitCount(normalized)
+	firstDigits := makeRegex(areaCode)
+	numberLength := len(normalized)
+	firstDigitsLength := 3
 
-  normalized := normalize(phoneNumber)
-  areaCode := areaCodeDigitCount(normalized)
-  firstDigits := makeRegex(areaCode)
+	voicemails := map[string]bool{
+		"333": true,
+		"222": true,
+		"888": true,
+	}
 
-  voicemails := map[string]bool{
-    "333": true,
-    "222": true,
-    "888": true,
-  }
+	if voicemails[normalized] {
+		return "Röstbrevlåda"
+	}
 
-  if voicemails[normalized] {
-    return "Röstbrevlåda"
-  }
+	switch areaCode {
+	case 2:
+		if numberLength == 8 {
+			firstDigitsLength = 2
+		} else if numberLength == 10 {
+			firstDigitsLength = 10
+		}
 
-  switch areaCode {
-  case 2:
-    firstDigitsLength := 3
+		return normalFormat(firstDigits(firstDigitsLength), normalized)
+	case 3:
+		if numberLength == 9 {
+			firstDigitsLength = 2
+		}
 
-    if len(normalized) == 8 {
-      firstDigitsLength = 2
-    } else if len(normalized) == 10 {
-      firstDigitsLength = 10
-    }
+		if numberLength == 8 {
+			return shortFormat(areaCode, normalized)
+		}
 
-    result = firstDigits(firstDigitsLength).ReplaceAllString(normalized, "$1-$2 $3 $4")
-  case 3:
-    firstDigitsLength := 3
+		return normalFormat(firstDigits(firstDigitsLength), normalized)
+	default:
+		if numberLength == 9 {
+			return shortFormat(areaCode, normalized)
+		}
 
-    if len(normalized) == 9 {
-      firstDigitsLength = 2
-    }
-
-    if len(normalized) == 8 {
-      result = regexp.MustCompile(`^(\d{3})(\d{3})(\d{2})$`).ReplaceAllString(normalized, "$1-$2 $3")
-    } else {
-      result = firstDigits(firstDigitsLength).ReplaceAllString(normalized, "$1-$2 $3 $4")
-    }
-  case 4:
-    if len(normalized) == 9 {
-      result = regexp.MustCompile(`^(\d{4})(\d{3})(\d{2})$`).ReplaceAllString(normalized, "$1-$2 $3")
-    } else {
-      result = firstDigits(2).ReplaceAllString(normalized, "$1-$2 $3 $4")
-    }
-  }
-
-  return result
+		return normalFormat(firstDigits(2), normalized)
+	}
 }
